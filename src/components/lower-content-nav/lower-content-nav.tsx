d4f435ca7d9ebaf0a10e760993b3b0d3666028be
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-09-22 16:51:59 -0500

    link lower navigation

--- src/components/lower-content-nav/lower-content-nav.tsx
+++ src/components/lower-content-nav/lower-content-nav.tsx
@@ -5,6 +5,7 @@ import { href } from '../../stencil-router-v2';
 @Component({
   tag: 'lower-content-nav',
   styleUrl: 'lower-content-nav.css',
+  scoped: true,
 })
 export class LowerContentNav {
   @Prop() navigation: PageNavigation;
@@ -17,15 +18,25 @@ export class LowerContentNav {
     return (
       <Host role="navigation">
         {n.previous?.url ? (
-          <a {...href(n.previous.url)} class="pull-left btn btn--secondary">
-            {n.previous.title && n.previous.title.length < 32
-              ? n.previous.title
-              : 'Back'}
+          <a {...href(n.previous.url)} class="nav-previous">
+            <div class="direction">Previous</div>
+            <div>
+              <svg viewBox="0 0 512 512">
+                <path d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z"></path>
+              </svg>
+              <span>{n.previous.title}</span>
+            </div>
           </a>
         ) : null}
         {n.next?.url ? (
-          <a {...href(n.next.url)} class="pull-right btn btn--primary">
-            {n.next.title && n.next.title.length < 32 ? n.next.title : 'Next'}
+          <a {...href(n.next.url)} class="nav-next">
+            <div class="direction">Next</div>
+            <div>
+              <span>{n.next.title}</span>
+              <svg viewBox="0 0 512 512">
+                <path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path>
+              </svg>
+            </div>
           </a>
         ) : null}
       </Host>
