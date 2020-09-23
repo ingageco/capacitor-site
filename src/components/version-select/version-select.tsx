cf71ad4f8c78e32f90096e2f5cac8c49bdbbfa58
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-09-23 00:01:38 -0500

    init version select

--- src/components/version-select/version-select.tsx
+++ src/components/version-select/version-select.tsx
@@ -1,21 +1,53 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, State, Listen } from '@stencil/core';
 import { href } from '../../stencil-router-v2';
 
 @Component({
   tag: 'version-select',
   styleUrl: 'version-select.scss',
+  scoped: true,
 })
 export class VersionSelect {
+  @State() expanded = false;
+
+  @Listen('click', { target: 'window' })
+  closeSelect() {
+    if (this.expanded) {
+      this.expanded = false;
+    }
+  }
+
+  openSelect = (ev: UIEvent) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+    this.expanded = !this.expanded;
+  };
+
   render() {
     return (
       <Host role="navigation" aria-label="Documentation Version Selector">
-        <a {...href('/docs')} class="version-selected">
+        <a
+          {...href('/docs')}
+          aria-label="Version 3 Docs"
+          class="version-selected"
+          onClick={this.openSelect}
+        >
           <span>v3</span>
           <ion-icon name="chevron-down-outline" />
         </a>
-        <div class="version-selector">
-          <a {...href('/docs')}>v3</a>
-          <a {...href('/docs')}>v2</a>
+        <div class="version-selector" hidden={!this.expanded}>
+          <a {...href('/docs')} aria-label="Version 3 Docs">
+            v3
+          </a>
+          <a {...href('/docs')} aria-label="Version 2 Docs">
+            v2
+          </a>
+          <a
+            href="https://github.com/ionic-team/capacitor/releases"
+            target="_blank"
+            class="releases"
+          >
+            All Releases
+          </a>
         </div>
       </Host>
     );
